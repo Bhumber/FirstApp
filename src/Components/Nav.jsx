1618ddc449d6b030208bd8bea6ff8dd6d6230da1
Никита Яковлев <hardmuscer@gmail.com>
2022-04-11 12:10:02 +0500

    i add test tag

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -8,6 +8,7 @@ const Nav = () => {
           <div><a href="#">News</a></div>
           <div><a href="#">Music</a></div>
           <div><a href="#">Settings</a></div>
+          <div><a href="#">Test</a></div>
         </nav>
     );
 }
