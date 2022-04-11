0a5e2c4afb803f32297d03252c96032cc3c20405
Никита Яковлев <hardmuscer@gmail.com>
2022-04-11 11:56:25 +0500

    Firsr Commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+          <div>LoGo</div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+          <div>
+            <img src='https://www.ubackground.com/_ph/17/709425269.jpg' />
+          </div>
+          <div>ava+description</div>
+          <div>My posts
+            <div>New post</div>
+          </div>
+          
+          <div>Post1</div>
+          <div>POst2</div>
+          
+        </div>
+    );
+}
+export default Profile;
